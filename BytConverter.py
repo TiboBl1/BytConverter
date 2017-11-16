2f220426124e028efcb0377edbcaf25153764c4d
TiboBl1 <32728780+TiboBl1@users.noreply.github.com>
2017-11-16 20:48:02 +0100

    v1
    :)

--- BytConverter.py
+++ BytConverter.py
@@ -9,7 +9,7 @@ print("'GB' as Gigabyte")
 print("'TB' as Terabyte")
 print("")
 
-in_num = input("Source Number: ")
+in_num = input("Source Value: ")
 s_unit = str(input("Source Unit: "))
 b = str("b")
 B = str("B")
@@ -35,6 +35,61 @@ if s_unit in [b, B, kB, MB, GB, TB]:
                 print("Result: ", ((((int(in_num) / 8) / 1024) / 1024) / 1024), str(d_unit))
             elif d_unit == TB:
                 print("Result: ", (((((int(in_num) / 8) / 1024) / 1024) / 1024) / 1024), str(d_unit))
+        elif s_unit == B:
+            if d_unit == b:
+                print("Result: ", (int(in_num) * 8), str(d_unit))
+            elif d_unit == kB:
+                print("Result: ", (int(in_num) / 1024), str(d_unit))
+            elif d_unit == MB:
+                print("Result: ", ((int(in_num) / 1024 / 1024), str(d_unit)))
+            elif d_unit == GB:
+                print("Result: ", (((int(in_num) / 1024) / 1024) / 1024), str(d_unit))
+            elif d_unit == TB:
+                print("Result: ", ((((int(in_num) / 1024) / 1024) / 1024) / 1024), str(d_unit))
+        elif s_unit == kB:
+            if d_unit == b:
+                print("Result: ", ((int(in_num) * 1024) * 8), str(d_unit))
+            elif d_unit == B:
+                print("Result: ", (int(in_num) * 1024), str(d_unit))
+            elif d_unit == MB:
+                print("Result: ", ((int(in_num) / 1024), str(d_unit)))
+            elif d_unit == GB:
+                print("Result: ", ((int(in_num) / 1024) / 1024), str(d_unit))
+            elif d_unit == TB:
+                print("Result: ", (((int(in_num) / 1024) / 1024) / 1024), str(d_unit))
+        elif s_unit == MB:
+            if d_unit == b:
+                print("Result: ", (((int(in_num) * 1024) * 1024) * 8), str(d_unit))
+            elif d_unit == B:
+                print("Result: ", ((int(in_num) * 1024) * 1024), str(d_unit))
+            elif d_unit == kB:
+                print("Result: ", ((int(in_num) * 1024), str(d_unit)))
+            elif d_unit == GB:
+                print("Result: ", (int(in_num) / 1024), str(d_unit))
+            elif d_unit == TB:
+                print("Result: ", ((int(in_num) / 1024) / 1024), str(d_unit))
+        elif s_unit == GB:
+            if d_unit == b:
+                print("Result: ", ((((int(in_num) * 1024) * 1024) * 1024) * 8), str(d_unit))
+            elif d_unit == B:
+                print("Result: ", (((int(in_num) * 1024) * 1024) * 1024), str(d_unit))
+            elif d_unit == kB:
+                print("Result: ", (((int(in_num) * 1024) * 1024), str(d_unit)))
+            elif d_unit == MB:
+                print("Result: ", (int(in_num) * 1024), str(d_unit))
+            elif d_unit == TB:
+                print("Result: ", (int(in_num) / 1024), str(d_unit))
+        elif s_unit == TB:
+            if d_unit == b:
+                print("Result: ", (((((int(in_num) * 1024) * 1024) * 1024) * 1024) * 8), str(d_unit))
+            elif d_unit == B:
+                print("Result: ", ((((int(in_num) * 1024) * 1024) * 1024) * 1024), str(d_unit))
+            elif d_unit == kB:
+                print("Result: ", ((((int(in_num) * 1024) * 1024) * 1024), str(d_unit)))
+            elif d_unit == MB:
+                print("Result: ", ((int(in_num) * 1024) * 1024), str(d_unit))
+            elif d_unit == GB:
+                print("Result: ", (int(in_num) * 1024), str(d_unit))
     else: print("Bad Unit! Exiting...")
     exit()
 else: print("Bad Unit! Exiting...")
